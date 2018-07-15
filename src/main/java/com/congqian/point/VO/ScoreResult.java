e0d20749e19160e7c319d98edeaea9d7c5de2b7d
Congboss <congqian62@126.com>
2018-07-15 10:08:13 +0800

    4

---
+++ src/main/java/com/congqian/point/VO/ScoreResult.java
@@ -0,0 +1,10 @@
+package com.congqian.point.VO;
+
+/**
+ * @program: point
+ * @description:
+ * @author: Cong.Qian
+ * @create: 2018-07-15 10:06
+ **/
+public class ScoreResult {
+}
---
+++ src/main/java/com/congqian/point/service/UserService.java
@@ -0,0 +1,13 @@
+package com.congqian.point.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: point
+ * @description:
+ * @author: Cong.Qian
+ * @create: 2018-07-15 10:06
+ **/
+@Service
+public class UserService {
+}
