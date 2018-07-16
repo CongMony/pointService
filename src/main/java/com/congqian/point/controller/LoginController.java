ce37f3e3281233239156a0536eef9c84de900646
Congboss <congqian62@126.com>
2018-07-16 12:27:12 +0800

    Update LoginController.java

--- src/main/java/com/congqian/point/controller/LoginController.java
+++ src/main/java/com/congqian/point/controller/LoginController.java
@@ -1,10 +1,22 @@
 package com.congqian.point.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 /**
  * @program: point
  * @description:
  * @author: Cong.Qian
  * @create: 2018-07-15 00:33
  **/
+@Controller
 public class LoginController {
+    @RequestMapping("/login")
+    public ModelAndView Login() {
+        return new ModelAndView("/concept/login");
+    }
+
+    @RequestMapping("/loginOut")
+    public ModelAndView cancel() {
+        return new ModelAndView("/concept/confirm");
+    }
 }
